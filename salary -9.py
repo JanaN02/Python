88fe6a6baeb97a6e5b9249942b13e68dd5ba479d
Jana N <123320392+JanaN02@users.noreply.github.com>
2023-05-17 20:41:35 +0530

    Update salary -9.py

--- salary -9.py
+++ salary -9.py
@@ -3,9 +3,19 @@
 a=int(input("Enter your salary amount : "))
 b=int(input("Enter your year of service : "))
 
-if(b>10):
+if(b>=10):
+    x=a*(10/100)
+    y=a+x
     print("10% bonus for the employe")
+    print("The bounus amount is : ",y) 
 elif(b>=6 and b<10):
+    x=a*(8/100)
+    y=a+x
     print("8% bonus for the employe")
+    print("The bounus amount is : ",y)
+elif(b<=5):
+    x=a*(5/100)
+    y=a+x
+    print("The bounus amount is : ",y) 
 else:
-    print("15% bonus for the employe")
+    print("Sorry, You didn't get bonus")
