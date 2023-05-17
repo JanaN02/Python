bfd88b852865bc880eef0c3eb7bb8d0c8f666b6a
Jana N <123320392+JanaN02@users.noreply.github.com>
2023-05-17 20:33:52 +0530

    Update Bus offer-7.py

--- Bus offer-7.py
+++ Bus offer-7.py
@@ -5,3 +5,9 @@ y=int(input("Enter current year : "))
 
 z=y-x
 print("Age of customer : ",z)
+
+if z>=60:
+  a=1020*(20/100)
+  print("The amni bus charge : ",a)
+else:
+  print("The amni bus charge : 1020")
