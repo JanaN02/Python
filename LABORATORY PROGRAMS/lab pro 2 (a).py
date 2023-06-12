9814cee687c1d5bd2b89423f2a8ec57db538705b
Jana N <123320392+JanaN02@users.noreply.github.com>
2023-06-12 21:45:34 +0530

    Update lab pro 2 (a).py

--- LABORATORY PROGRAMS/lab pro 2 (a).py
+++ LABORATORY PROGRAMS/lab pro 2 (a).py
@@ -1,18 +1,19 @@
-a=int(input("Enter number elements in list : "))
+n=int(input("Enter number of elements in list : "))
+a=list()
 
-I=list()
-for i in range(0,a):
+for i in range(0,n):
     b=int(input("Enter the value : "))
-    I.append(b)
-print("Original list : ",I)
-b=0
-c=I[:]
-c.sort()
+    a.append(b)
+print("Original list : ",a)
 
-if (c==I):
-    b=1
+c=int(input("Enter the element to be found : "))
 
-if (b):
-    print("Yes, the list is in assending order")
-else:
-    print("No, the list is not in assening order")
+posi=[]
+nega=[]
+d=len(n)
+for i in range(0,d):
+    if n[i]==a:
+        posi.append(i)
+        nega.append(-len(n)+i)
+print("Positive index: ",posi)
+print("Negative index : ",nega)
